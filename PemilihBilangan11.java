19ff84b5bef81df7bef5a40821c87b9a86d53e8f
saya <geraldiakmal612@gmail.com>
2024-09-25 07:37:23 +0700

    penambahan program

---
+++ PemilihBilangan11.java
@@ -0,0 +1,23 @@
+//mengimpor modul scanner
+import java.util.Scanner;
+
+
+class PemilihanBilangan11{
+    public static void main(String[] args) {
+        //konfigurasi scanner
+        Scanner inputan = new Scanner(System.in);
+
+        //mulai eksekusi program
+        System.out.print("Masukan Angka : ");
+        int angka = inputan.nextInt();
+        if (angka%2 == 0){
+            System.out.println("Angka"+angka+"Termasuk Bilangan genap");
+        }
+
+        else {
+            System.out.print("Angka "+angka+" Termasuk Bilangan Ganjil");
+        }
+        
+
+    }
+}
